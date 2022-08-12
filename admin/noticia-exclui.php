be5dad8aa7f683f3d9eada3fa0a8fd62826ba25d
Ignacio Neto <ignacio.cunha.neto@gmail.com>
2022-08-12 12:01:03 -0300

    Melhoria de segurança
    Melhoria de segurança no excluir notícia.

--- admin/noticia-exclui.php
+++ admin/noticia-exclui.php
@@ -1,5 +1,6 @@
 <?php
 
+use Microblog\Noticia;
 use Microblog\ControleDeAcesso;
 
 require_once "../vendor/autoload.php";
@@ -7,3 +8,20 @@ require_once "../vendor/autoload.php";
 // Para proteger a página
 $sessao = new ControleDeAcesso;
 $sessao->verificaAcesso();
+
+// Criamos um objeto para poder acessar os recursos da classe
+$noticia = new Noticia; // Não esqueça do autoload e do namespace
+
+// Obtemos o ID da url e o passamos para o setter
+$noticia->setId($_GET['id']);
+
+$noticia->usuario->setId($_SESSION['id']);
+$noticia->usuario->setTipo($_SESSION['tipo']);
+
+// Só então executamos o método de exclusão
+$noticia->excluirNoticia();
+
+// Após excluir, redirecionamos para a página de lista de usuários
+header("location:noticias.php");
+
+// A idéia aqui é excluir direto (sem mensagens)
