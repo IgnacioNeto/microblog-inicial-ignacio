e1d6691a1995178a2d8f09037fdd01f78da74ca4
Ignacio Neto <ignacio.cunha.neto@gmail.com>
2022-08-19 11:31:47 -0300

    Ajuste caminhos Bootstrap (admin)

--- inc/cabecalho-admin.php
+++ inc/cabecalho-admin.php
@@ -24,12 +24,12 @@ $pagina = basename($_SERVER['PHP_SELF']);
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Microblog</title>
 
-<link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
 <!-- ONLINE -->
 <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
 <!-- ONLINE -->
 <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"> -->
-<link rel="stylesheet" href="vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
+<link rel="stylesheet" href="../vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
 <link rel="stylesheet" href="../css/style.css">
 
 </head>
